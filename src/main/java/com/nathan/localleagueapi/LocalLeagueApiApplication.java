54e695ba19911bfc8933df46e3e84203b3d0234d
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-15 17:45:17 +0300

    project dependencies setup and structure

---
+++ src/main/java/com/nathan/localleagueapi/LocalLeagueApiApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.localleagueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalLeagueApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalLeagueApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathan/localleagueapi/LocalLeagueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.localleagueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalLeagueApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
