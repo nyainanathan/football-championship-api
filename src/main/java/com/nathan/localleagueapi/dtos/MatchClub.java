33e43122525d945c71dc39a7144656d0274e4053
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-16 00:11:09 +0300

    Feat: MatchClub DTO

---
+++ src/main/java/com/nathan/localleagueapi/dtos/MatchClub.java
@@ -0,0 +1,11 @@
+package com.nathan.localleagueapi.dtos;
+
+import java.util.List;
+
+public class MatchClub {
+    private String id;
+    private String name;
+    private String acronym;
+    private int score;
+    private List<Scorer> scorers;
+}
