055ed93fd1c57aaae9d46afe64197ba6c8c9996f
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-23 18:43:22 +0300

    Feat: getScorers method to match repo to get goals from a match

--- src/main/java/com/nathan/localleagueapi/repository/MatchRepo.java
+++ src/main/java/com/nathan/localleagueapi/repository/MatchRepo.java
@@ -1,13 +1,17 @@
 package com.nathan.localleagueapi.repository;
 
 import com.nathan.localleagueapi.model.match.Match;
+import com.nathan.localleagueapi.model.match.Scorer;
+import com.nathan.localleagueapi.model.player.PlayerMinimumInfo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -33,5 +37,32 @@ public class MatchRepo {
             preparedStatement.executeBatch();
         }
 
+    }
+
+    public List<Scorer> getScorers(String matchId)  throws SQLException {
+        List<Scorer> scorers = new ArrayList<>();
+        try{
+            String sql = "select g.*, p.id as player_id, p.name, p.number from goals as g inner join players as p on g.player_id = p.id where g.match_id = ?";
+            Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, matchId);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                scorers.add(new Scorer(
+                        new PlayerMinimumInfo(
+                             rs.getString("player_id"),
+                             rs.getString("name"),
+                             rs.getInt("number")
+                        ),
+                        rs.getInt("minute_of_goal"),
+                        rs.getBoolean("own_goal"),
+                        rs.getString("club_id")
+                ));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            throw e;
+        }
+        return scorers;
     }
 }
