42020e707f7f1f49a66463416f116af58de94e40
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-24 16:08:08 +0300

    Feat: PlayerStat DTO

---
+++ src/main/java/com/nathan/localleagueapi/dto/PlayerStat.java
@@ -0,0 +1,11 @@
+package com.nathan.localleagueapi.dto;
+
+import com.nathan.localleagueapi.model.DurationUnit;
+
+public class PlayerStat {
+    private String playerId;
+    private String season;
+    private int scoredGoals;
+    private int playingTime;
+    private DurationUnit durationUnit;
+}
